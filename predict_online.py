398172f22161be01268412adf637c64e255cf9a2
Artem Smetanin <webstermaster777@gmail.com>
2023-04-07 14:40:05 +0300

    update: add power limit

--- predict_online.py
+++ predict_online.py
@@ -32,6 +32,9 @@ def main():
     EXP_SMOOTH = config['EXP_SMOOTH']
     DOUBLE_EXP_SMOOTH = config['DOUBLE_EXP_SMOOTH']
     ROLLING_MEAN_WINDOW = config['ROLLING_MEAN_WINDOW']
+    
+    POWER_ID = config['POWER_ID']
+    POWER_LIMIT = config['POWER_LIMIT']
 
     client = clickhouse_connect.get_client(host='10.23.0.177', username='default', password='asdf')
     model_list = []
@@ -44,51 +47,56 @@ def main():
         scaler = joblib.load(scaler_save)
         scaler_list.append(scaler)
         model_list.append(model)
-
+    prev_df = client.query_df(f"SELECT * FROM lstm_group{i}").tail(1)
     try:
         while True:
             df = client.query_df(QUERY_DF).tail(1)
-            df = df.iloc[:, :-1]
-            if ROLLING_MEAN:
-                rolling_mean = df.rolling(window=ROLLING_MEAN_WINDOW).mean()
-            if EXP_SMOOTH:
-                for i in df.columns:
-                    df[str(i)] = exponential_smoothing(df[str(i)].to_numpy(), alpha=0.2)
-            if DOUBLE_EXP_SMOOTH:
-                for i in df.columns:
-                    df[str(i)] = double_exponential_smoothing(df[str(i)].to_numpy(), alpha=0.02, beta=0.09)
+            print(df[POWER_ID][0])
+            if df[POWER_ID][0]>POWER_LIMIT:
+                df = df.iloc[:, :-1]
+                if ROLLING_MEAN:
+                    rolling_mean = df.rolling(window=ROLLING_MEAN_WINDOW).mean()
+                if EXP_SMOOTH:
+                    for i in df.columns:
+                        df[str(i)] = exponential_smoothing(df[str(i)].to_numpy(), alpha=0.2)
+                if DOUBLE_EXP_SMOOTH:
+                    for i in df.columns:
+                        df[str(i)] = double_exponential_smoothing(df[str(i)].to_numpy(), alpha=0.02, beta=0.09)
 
-            groups = pd.read_csv(KKS, sep=';')
-            group_list = []
-            sum = 0
-            for i in range(0, NUM_GROUPS):
-                group = groups[groups['group'] == i]
-                if i != 0:
-                    group = group.append(groups[groups['group'] == 0])
-                sum += len(group)
-                if len(group) == 0:
-                    continue
-                group = df[group['External Tags']]
-                scaler = scaler_list[i]
-                scaled_data = pd.DataFrame(
-                    data=scaler.transform(group),
-                    columns=group.columns)
-                group_list.append(scaled_data)
+                groups = pd.read_csv(KKS, sep=';')
+                group_list = []
+                sum = 0
+                for i in range(0, NUM_GROUPS):
+                    group = groups[groups['group'] == i]
+                    if i != 0:
+                        group = group.append(groups[groups['group'] == 0])
+                    sum += len(group)
+                    if len(group) == 0:
+                        continue
+                    group = df[group['External Tags']]
+                    scaler = scaler_list[i]
+                    scaled_data = pd.DataFrame(
+                        data=scaler.transform(group),
+                        columns=group.columns)
+                    group_list.append(scaled_data)
 
-            loss_list = []
-            for i in range(0, len(group_list)):
-                X = group_list[i].to_numpy()
-                model = model_list[i]
-                len_size = get_len_size(LAG, X.shape[0])
-                X = X[:len_size].reshape(int(X.shape[0] / LAG), int(LAG), X.shape[1])
-                preds = model.predict(X, verbose=1)
-                preds = preds[:, 0, :]
-                yhat = X[:, 0, :]
-                loss = abs(yhat - preds)
-                loss_list += loss.tolist()
-                data = pd.DataFrame(loss, columns=group_list[i].columns)
-                client.insert_df(f"lstm_group{i}", data)
-            time.sleep(5)
+                loss_list = []
+                for i in range(0, len(group_list)):
+                    X = group_list[i].to_numpy()
+                    model = model_list[i]
+                    len_size = get_len_size(LAG, X.shape[0])
+                    X = X[:len_size].reshape(int(X.shape[0] / LAG), int(LAG), X.shape[1])
+                    preds = model.predict(X, verbose=1)
+                    preds = preds[:, 0, :]
+                    yhat = X[:, 0, :]
+                    loss = abs(yhat - preds)
+                    loss_list += loss.tolist()
+                    data = pd.DataFrame(loss, columns=group_list[i].columns)
+                    client.insert_df(f"lstm_group{i}", data)
+                    prev_df = data
+                time.sleep(5)
+            else:
+                client.insert_df(f"lstm_group{i}", prev_df)
     finally:
         client.disconnect()
         print("disconnected")
