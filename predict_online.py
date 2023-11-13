35fc5aa274812b4eff244cb72e73e80f7e991c3c
Artem Smetanin <38701834+a-smetanin@users.noreply.github.com>
2023-11-13 15:57:39 +0300

    Update: add first_start and delete table arg

--- predict_online.py
+++ predict_online.py
@@ -28,6 +28,11 @@ def main():
     parser.add_argument('--config_path', type=str, default='')
     parser.add_argument('--csv_kks', type=bool, default=False)
     parser.add_argument("-v", "--version", action="store_true", help="вывести версию программы")
+    # parser.add_argument('--config_path', type=str, default='')
+    parser.add_argument('--first_start', action="store_true")
+    parser.add_argument('--delete_group_table', action="store_true")
+    
+    # parser.add_argument("-v", "--version", action="store_true", help="вывести версию программы")
     opt = parser.parse_args()
     if opt.version:
         print("Версия predict online:", VERSION)
@@ -47,7 +52,6 @@ def main():
     NUM_GROUPS = config['NUM_GROUPS']
     LAG = config['LAG']
     QUERY_DF = config['QUERY_DF']
-    CREATE_TABLE = config['CREATE_TABLE']
 
     ROLLING_MEAN = config['ROLLING_MEAN']
     EXP_SMOOTH = config['EXP_SMOOTH']
@@ -62,6 +66,8 @@ def main():
     WINDOW_SIZE = config['WINDOW_SIZE']
     TRESHOLD_ANOMALY = config['TRESHOLD_ANOMALY']
     CONTINUE_COUNT = config['CONTINUE_COUNT']
+    
+    TIME_BETWEEN_PREDICT = config['TIME_BETWEEN_PREDICT']
 
     client = clickhouse_connect.get_client(host='10.23.0.87', username='default', password='asdf')
     model_list = []
@@ -101,7 +107,7 @@ def main():
                 else:
                     logger.debug('READ CSV KKS')
                     groups = pd.read_csv(KKS, sep = ';')
-                     
+                        
                 # print(groups['group'])
                 group_list = []
                 sum = 0
@@ -118,6 +124,7 @@ def main():
                         data=scaler.transform(group),
                         columns=group.columns)
                     group_list.append(scaled_data)
+                    # print(i)
                     # print(group)
 
                 loss_list = []
@@ -126,16 +133,22 @@ def main():
                     model = model_list[i]
                     len_size = get_len_size(LAG, X.shape[0])
                     X = X[:len_size].reshape(int(X.shape[0] / LAG), int(LAG), X.shape[1])
+                    # logger.debug(X)
                     preds = model.predict(X, verbose=1)
+                    # logger.debug(preds)
                     preds = preds[:, 0, :]
                     yhat = X[:, 0, :]
                     loss = abs(yhat - preds)
                     loss_mean = np.mean(np.abs(yhat - preds), axis=1)
                     loss_list += loss.tolist()
+                    logger.debug(loss_list)
+                    # print(loss)
                     data = pd.DataFrame(loss, columns=group_list[i].columns)
+                    print(data)
                     data['timestamp'] = time_df
                     
-                    forecast_df = client.query_df(f"SELECT * FROM lstm_group{i}").tail(TRAIN_LEN_FORECAST)
+                    if not opt.first_start:
+                        forecast_df = client.query_df(f"SELECT * FROM lstm_group{i}").tail(TRAIN_LEN_FORECAST)
                     # logger.debug(forecast_df)
                     try: 
                         if config['SCALER_LOSS_NAME'] == 'minmax':
@@ -145,44 +158,56 @@ def main():
                             scaler_ = scipy.stats.rv_histogram(scaler_loss_list[i])
                             # logger.debug(scaler_loss_list[i])
                             data['target_value'] = scaler_.cdf(loss_mean) * 100
-    
+
                     except Exception as e:
                         logger.error(e)
                         
                     logger.info(f"Target value: {data['target_value']}")
-                    # logger.info (loss_mean)
+                    logger.info (loss_mean)
                     
-                    predict_val = make_forecast(train_data = np.array(forecast_df['target_value']), len_forecast = LEN_FORECAST, window_size = WINDOW_SIZE )
-                    treshold = TRESHOLD_ANOMALY
-                    count = 0
-                    prob = np.round(data['target_value'],0)
-                    continue_count = 0
                     
-                    for val in predict_val:
-                        count += 1
-                        if val > treshold:
-                            continue_count += 1
-                        else:
-                            # count = 0
-                            continue_count = 0
-                        if continue_count == CONTINUE_COUNT:
-                            continue_count = 0
-                            break
-                    if count<len(predict_val):
-                        logger.info
-                    print(count)
-                    print(treshold)
-                    if count == LEN_FORECAST:
-                        count = 8640
-                    data['prob'] = prob
-                    data['count'] = count
-                    count = 0
-                    # logger.info(prob)
-                    # try:
-                    #     client.command(f'DROP TABLE lstm_group{i}')  
-                    # except Exception as e:
-                    #     logger.error(e)
-                    if CREATE_TABLE:
+                    try:
+                        predict_val = make_forecast(train_data = np.array(forecast_df['target_value']), len_forecast = LEN_FORECAST, window_size = WINDOW_SIZE )
+                        treshold = TRESHOLD_ANOMALY
+                        prob =0 
+                        count = 0
+                        prob = np.round(data['target_value'],0)
+                        continue_count = 0
+                        
+                        for val in predict_val:
+                            count += 1
+                            if val > treshold:
+                                continue_count += 1
+                            else:
+                                # count = 0
+                                continue_count = 0
+                            if continue_count == CONTINUE_COUNT:
+                                continue_count = 0
+                                break
+                        if count<len(predict_val):
+                            logger.info
+                        print(count)
+                        print(treshold)
+                        if count == LEN_FORECAST:
+                            count = 8640
+                        data['prob'] = prob
+                        data['count'] = count
+                        count = 0
+                    except Exception as e:
+                        logger.warning(e)
+                        predict_val  = 0
+                        prob = 0 
+                        count = 0
+                        logger.info(prob)
+                    
+                    if opt.delete_group_table:
+                        try:
+                            logger.info('DROP GROUP TABLE')
+                            client.command(f'DROP TABLE lstm_group{i}')  
+                            sys.exit()
+                        except Exception as e:
+                            logger.error(e)
+                    if opt.first_start:
                         col_str = '"timestamp"' +' ' +'DateTime, '
                         col_str += '"target_value"' + 'Float64, ' 
                         col_str += '"prob"' + 'Float64, '
@@ -194,7 +219,10 @@ def main():
                     client.insert_df(f"lstm_group{i}", data)
                     prev_df = data
                     logger.info(f'LOSS DATA: \n {data}')
-                time.sleep(300)
+                    if opt.first_start:
+                        sys.exit()
+                    
+                time.sleep(TIME_BETWEEN_PREDICT)
                 # count = 0
                 # break
             else:
